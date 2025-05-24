9c21f6bfd424d84818f1a20ab2b1863d51753320
Mario Portillo Hernaiz <portillomario407@gmail.com>
2025-05-24 16:46:05 +0100

    Improve sidebar

--- app/page.tsx
+++ app/page.tsx
@@ -5,7 +5,7 @@ import Link from "next/link";
 export default function Home() {
   return (
     <div className="flex flex-col md:flex-row min-h-screen">
-      <h1 className="text-4xl sm:text-3xl font-bold tracking-tight block sm:hidden pt-10 mx-auto">TIYASHA COREA</h1>
+      <h1 className="text-4xl sm:text-3xl font-bold tracking-tight block sm:hidden pt-16 mx-auto">TIYASHA COREA</h1>
       <div className="block sm:hidden flex justify-center space-x-6 text-gray-500 p-4">
         <Link href="/about" className="block py-1 hover:text-black">
           About / Contact
